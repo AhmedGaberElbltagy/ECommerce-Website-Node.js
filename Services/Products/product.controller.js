7e5bec95df139ada43cf7b21981447aaf64975f6
AhmedGaberElbltagy <ahmedelbltagy1999@gmail.com>
2024-04-07 05:11:54 +0200

    adding new features

--- Services/Products/product.controller.js
+++ Services/Products/product.controller.js
@@ -35,6 +35,7 @@ const getAllProducts = async ( req , res , next) => {
         if (!Products) {
             throw new ErrorHandler(404 ,"Products not found")
         }
+        
         const productsWithReviews = Products.map(product => {
             return { 
                 product,
@@ -49,5 +50,76 @@ const getAllProducts = async ( req , res , next) => {
     }
 }
 
+const getSingleProduct =async (req ,res ,next) => {
+try{
+    const productId = req.params.id;
+    const product = await Product.findById(productId).populate('productReviews');
+    
+    if (!product) {
+        throw new ErrorHandler(404, "not found")
+    }
+    const reviews = product.productReviews
+    return response( true , 200 , {product,reviews} , res )
+} catch (error) {
+    next(error)
+}
+}
+const updateProduct = async ( req , res , next) => {
+try {
+    const productId = req.params.id;
+    const product = await Product.findById( productId)
+    if (!product) {
+        throw new ErrorHandler( 404 , "not found")
+    }
+    if (req.file) {
+        let publicId = product.imageCover.split('/').pop().split('.')[0]
+        await cloudinaryConfig().uploader.destroy(publicId, (err, result) => {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log(result);
+            }
+        });
 
-module.exports = {addProduct,getAllProducts}
+        let image = await cloudinaryConfig().uploader.upload(req.file.path, {
+            folder: 'Ecommerce/product'
+        })
+        req.body.imageCover = image.url
+    }
+    let updatedproduct = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    if (!updateProduct) {
+        throw new ErrorHandler(409 , " failed to update")
+    }
+    return response( true , 200 , { updatedproduct } , res )
+}catch (error) {
+    next(error)
+}
+}
+
+const deleteProduct = async ( req , res , next ) => {
+try {
+     const productId = req.params.id 
+     const product = await Product.findById(productId)
+     if (!product) {
+        throw new ErrorHandler(404 , "product not found")
+     } 
+        let publicId = product.imageCover.split('/').pop().split('.')[0]
+        await cloudinaryConfig().uploader.destroy(publicId, async (err, result) => {
+        if (err) {
+            console.error(err);
+            throw new ErrorHandler(500, "Failed to delete image from Cloudinary");
+        } else {
+            console.log(result);
+            await Product.deleteOne({ _id: productId });
+            return response( true , 200 , null, res)
+        }
+    });
+    
+
+}catch (error) {
+    next(error)
+}
+}
+
+
+module.exports = { addProduct , getAllProducts , getSingleProduct , updateProduct , deleteProduct}
--- Services/Products/product.routes.js
+++ Services/Products/product.routes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router();
-const {addProduct ,getAllProducts }= require('./product.controller');
+const { addProduct , getAllProducts ,getSingleProduct,updateProduct ,deleteProduct }= require('./product.controller');
 const protectedRouter = require('../../Middlewares/Auth');
 const uploadFields = require('../../utils/fileUpload')
 
@@ -8,6 +8,12 @@ const uploadFields = require('../../utils/fileUpload')
 
 router.post('/addProduct',protectedRouter,uploadFields([{ name: 'imageCover', maxCount: 1},{ name: 'images',maxCount: 5}]) ,addProduct);
 
-router.get('/getAllProducts',getAllProducts)
+router.get('/getAllProducts',getAllProducts) ;
+
+router.get('/getSingleProduct/:id',getSingleProduct) ;
+
+router.put('/updateProduct/:id',updateProduct)
+
+router.delete('/deleteProduct/:id',deleteProduct)
 
 module.exports = router;
